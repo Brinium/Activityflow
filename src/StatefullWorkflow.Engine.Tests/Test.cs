f1edf1cd996ccc48326baf9af50ce684de55494e
Sam Bowman <ulsiks@gmail.com>
2016-03-16 13:40:59 +1100

    * StatefullWorkflow.sln: * Test.cs: * packages.config: * packages.config: * packages.config: * StatefullWorkflow.Engine.csproj: * StatefullWorkflow.Console.csproj: * StatefullWorkflow.DataAccess.csproj: * StatefullWorkflow.Engine.Tests.csproj:
    * packages.config: adding test and packages

---
+++ src/StatefullWorkflow.Engine.Tests/Test.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace StatefullWorkflow.Engine.Tests
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void TestCase()
+        {
+        }
+    }
+}
+
