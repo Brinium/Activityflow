166023b4336b125e7792c502a1fb414a892f25ce
Sam <Sam at GitHub>
2016-03-16 14:01:41 +1100

    test

--- src/StatefullWorkflow.Engine.Tests/Test.cs
+++ src/StatefullWorkflow.Engine.Tests/Test.cs
@@ -6,6 +6,13 @@ namespace StatefullWorkflow.Engine.Tests
     [TestFixture()]
     public class Test
     {
+        [TearDown]
+        public void TearDown()
+        {
+            //using console here just for sake of simplicity. 
+            Console.WriteLine(String.Format("{0}: {1}", TestContext.CurrentContext.Test.FullName, TestContext.CurrentContext.Result.Outcome));
+        }
+
         [Test()]
         public void TestCase()
         {
